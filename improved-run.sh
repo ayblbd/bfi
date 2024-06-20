ba3de7b2bf111695b53eb61fe0b04ccfa23c75fa
Ayoub Labiad <23561726+ayblbd@users.noreply.github.com>
2024-06-20 11:47:32 +0100

    Add files via upload

---
+++ improved-run.sh
@@ -0,0 +1,268 @@
+#!/bin/bash
+
+function log() {
+    local timestamp
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    printf "%s - %s\n" "$timestamp" "$*" | tee -a "$LOG_FILE" >&2
+}
+
+function to_lower() {
+    if [ -z "$1" ]; then
+        log "No string provided to convert to lowercase."
+        return 1
+    fi
+
+    local str="$1"
+    tr '[:upper:]' '[:lower:]' <<<"$str"
+}
+
+# check_env checks the environment and changes directory based on the environment.
+# It takes two arguments: the environment name and the home directory.
+function check_env() {
+    local environment="${1:-prod}"
+    local home_directory=$2
+    local environment_lowercase
+
+    # Convert the environment name to lowercase.
+    environment_lowercase=$(to_lower "$environment")
+
+    # Check if the environment name contains "dev" or "prod".
+    case "$environment_lowercase" in
+    "dev" | "rec")
+        log "Info: Environment set to 'dev'"
+        echo "dev"
+        ;;
+    "prd")
+        # Check if the home directory exists before trying to cd into it.
+        if [[ ! -d "$home_directory" ]]; then
+            log "Error: Directory '$home_directory' does not exist"
+            return 1
+        fi
+
+        cd "$home_directory" || return 1
+        log "Info: Changed directory to '$home_directory'"
+        echo "prod"
+        ;;
+    *)
+        log "Error: environment '$1' does not contain 'dev' or 'prd'"
+        return 2
+        ;;
+    esac
+}
+
+# check_job checks the job name and returns the corresponding script name.
+# It takes one argument: the job name.
+function check_job() {
+    local job_name="${1:-main}"
+    local job_name_lowercase
+
+    # Convert the job name to lowercase.
+    job_name_lowercase=$(to_lower "$job_name")
+
+    # Check the job name.
+    case "$job_name_lowercase" in
+    "main" | "bfi")
+        log "Info: Job set to 'bfi.py'"
+        echo "bfi.py"
+        ;;
+    "ods")
+        log "Info: Job set to 'ods.py'"
+        echo "ods.py"
+        ;;
+    *)
+        log "Error: job does not contain '$job_name'"
+        return 1
+        ;;
+    esac
+}
+
+# check_date checks if the provided date matches the format 'YYYY-MM-DD'.
+# It takes one argument: the date.
+function check_date() {
+    local partition_date="${1:-}"
+
+    # Check if a date was provided.
+    if [[ -n "$partition_date" ]]; then
+        # Check if the date matches the format 'YYYY-MM-DD'.
+        if date -d "$partition_date" >/dev/null 2>&1; then
+            log "Info: Date '$partition_date' is valid"
+            echo "$partition_date"
+        else
+            log "Error: partition_date '$partition_date' does not match format 'YYYY-MM-DD'"
+            return 1
+        fi
+    else
+        log "Info: No date provided, using default"
+    fi
+}
+
+# create_logs_directory creates a directory named 'logs' if it does not already exist.
+function create_logs_directory() {
+    log "Info: Ensuring logs directory exists..."
+    if ! mkdir -p logs; then
+        log "Error: Failed to create logs directory"
+        return 1
+    else
+        log "Info: Logs directory created or already exists"
+    fi
+}
+# clean_old_logs lists and deletes files older than 7 days in the logs directory.
+function clean_old_logs() {
+    local logs_directory="logs"
+
+    if [[ ! -d "$logs_directory" ]]; then
+        log "Error: Directory '$logs_directory' does not exist" >&2
+        return 1
+    fi
+
+    log "Info: Listing files older than 7 days in $logs_directory directory:"
+    find "$logs_directory" -type f -mtime +7 -ls
+
+    log "Info: Deleting files older than 7 days in $logs_directory directory:"
+    find "$logs_directory" -type f -mtime +7 -delete
+
+    log "Info: Old logs cleanup completed"
+}
+
+# log_file generates a log file name based on the provided job name and date.
+# It takes two arguments: the job name and the date.
+function log_file() {
+    local job_name=$1
+    local env=$2
+    local logs_directory="logs"
+    local log_file_name
+
+    # Remove the .py extension from the job name.
+    job_name=$(basename "$job_name" .py)
+
+    # Check if the logs directory exists before trying to create a log file in it.
+    if [[ ! -d "$logs_directory" ]]; then
+        log "Error: Directory '$logs_directory' does not exist" >&2
+        return 1
+    fi
+
+    # Generate the log file name.
+    log_file_name="${logs_directory}/${job_name}_${env}_$(date +%Y%m%d_%H%M%S).log"
+
+    log "Info: Log file '$log_file_name' will be created"
+    echo "$log_file_name"
+}
+
+# tar_gz_src creates a tar.gz archive of all .py files in the src directory.
+function tar_gz_src() {
+    local src_directory="src"
+    local archive_name="src.tar.gz"
+    local tar_error
+
+    # Check if the src directory exists before trying to find files in it.
+    if [[ ! -d "$src_directory" ]]; then
+        log "Error: Directory '$src_directory' does not exist" >&2
+        return 2
+    fi
+
+    log "Info: Found src directory '$src_directory'"
+
+    # Check if there are any .py files in the src directory.
+    if ! find "$src_directory" -name "*.py" -print -quit | grep -q .; then
+        log "Error: No .py files found in directory '$src_directory'" >&2
+        return 3
+    fi
+
+    log "Info: Found .py files in src directory"
+
+    # Create a tar.gz archive of all .py files in the src directory.
+    tar_error=$(find "$src_directory" -name "*.py" -printf '%P\n' | tar -czf "$archive_name" -C "$src_directory" -T - 2>&1)
+    if [[ $tar_error -ne 0 ]]; then
+        log "Error: Failed to create archive '$archive_name': $tar_error" >&2
+        return 4
+    fi
+
+    log "Info: Successfully created archive '$archive_name'"
+}
+
+function check_status() {
+    local status=$1
+    local function_name=$2
+
+    if [[ $status -ne 0 ]]; then
+        log "Error: $function_name failed with status $status" >&2
+        exit "$status"
+    fi
+}
+
+run_spark_submit() {
+    local job_name=$1
+    local partition_date=$2
+    local temp_log_file=$3
+    local log4j_setting=$4
+    local log4j_setting="-Dlog4j.configuration=file:log4j.properties"
+
+    (
+        spark-submit \
+            --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./venv/bin/python \
+            --master yarn \
+            --deploy-mode client \
+            --num-executors 2 \
+            --driver-memory 8g \
+            --executor-memory 4g \
+            --conf "spark.driver.extraJavaOptions=${log4j_setting}" \
+            --conf "spark.executor.extraJavaOptions=${log4j_setting}" \
+            --files log4j.properties \
+            --py-files src.tar.gz \
+            "$job_name" "$partition_date"
+    ) 2>&1 | tee -a "$temp_log_file"
+}
+
+cleanup() {
+    if [ -n "$temp_log_file" ] && [ -f "$temp_log_file" ]; then
+        if [ -z "$log_file" ]; then
+            log_file=$(log_file "$job_name" "$environment")
+        fi
+        mv "$temp_log_file" "$log_file"
+        unset temp_log_file
+    fi
+}
+
+function main() {
+    home_dir="/home/user/spark/bfi"
+    job_name=$1
+    environment=$2
+
+    temp_log_file=$(mktemp)
+    LOG_FILE="$temp_log_file"
+
+    # Set up a trap to ensure logs are saved on exit
+    trap cleanup EXIT
+
+    create_logs_directory
+    check_status $? "create_logs_directory"
+
+    log "Info: Starting main function"
+
+    # Validate job and environment
+    job_name=$(check_job "$job_name")
+    check_status $? "check_job"
+
+    environment=$(check_env "$environment" "$home_dir")
+    check_status $? "check_env"
+
+    partition_date=$(check_date "$3")
+    check_status $? "check_date"
+
+    tar_gz_src "$environment"
+    check_status $? "tar_gz_src"
+
+    log_file=$(log_file "$job_name" "$environment")
+    check_status $? "log_file"
+
+    clean_old_logs
+    check_status $? "clean_old_logs"
+
+    log "Info: Starting spark-submit command"
+
+    run_spark_submit "$job_name" "$partition_date" "$temp_log_file"
+
+    log "Info: Finished main function"
+}
+
+main "$@"
